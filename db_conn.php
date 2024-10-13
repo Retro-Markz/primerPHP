0abb17a6bc38497c36356bd78cade390e22fd85b
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-10-13 03:59:25 -0700

    todo list with php and mysql

---
+++ db_conn.php
@@ -0,0 +1,17 @@
+<?php
+    
+$sName = 'localhost';
+$uName = "root";
+$pass = 1234;
+$db_name = "todolist";
+
+try{
+    $conn = new PDO("mysql:host=$sname;db_name=$db_name;", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "DB connected!";
+}catch(PDOException $e){
+    echo "Connection failed  : " . $e->getMessage();
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>To do PHP</title>
+</head>
+<body>
+        <div class="main-section">
+            <div class="add-section">
+                <form action="">
+                    <input type="text" name="title" placeholder="this fied is required" required>
+                    <button type="submit">Add &nbsp; <span>&#43;</span></button>
+
+                </form>
+            </div>
+            <div class="show-todo-section">
+                <div class="todo-item">
+                    <input type="checkbox">
+                    <h2>This is Mark Dev</h2>
+                    <br>
+                    <small>created in 2024</small>
+                </div>
+                
+            </div>
+        </div>
+</body>
+</html>
